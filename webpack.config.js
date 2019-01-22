7a8af443ec6325f2617680ba2c224d3e4dc14ef0
priya suresh <priyasuresh@MacBook-Pro-5.local>
2019-01-22 00:04:32 -0800

    added loader for es2015

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['env', 'react'],
+          presets: ['env', 'es2015', 'react'],
+          plugins: ['transform-class-properties']
         }
       }, 
       {
